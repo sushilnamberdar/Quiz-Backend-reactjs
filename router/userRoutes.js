43cb5baa82463cf6aea712d46b0f80418e32dfaf
sushilnamberdar <sushilnamberdar000@gmail.com>
2024-08-15 08:48:39 +0530

    merge

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -5,6 +5,8 @@ const AdminDataController = require('../Controller/AdminDataController');
 const deleteTestResult = require('../Controller/deleteTestResult');
 const adauth = require('../middleware/adauth');
 const getQuizQuestions = require('../Controller/QuestionController');
+const AddQuestionController = require('../Controller/AddQuestionController');
+const Questionfilejson = require('../Controller/Questionfilejson');
 
 
 const router = express.Router();
@@ -16,4 +18,6 @@ router.post('/admin',adminLogin);
 router.get('/admindashboard',AdminDataController);
 router.delete('/deleteTestResult', adauth, deleteTestResult); 
 router.get('/questions',getQuizQuestions)
+router.post('/addquestion',AddQuestionController)
+router.post('/addjsonfile',Questionfilejson)
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ dbConnection();
 const cors = require('cors');
 
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json({
+        limit:'10mb'
+    }));
 
     const router = require('./router/userRoutes')
     app.use(router);
